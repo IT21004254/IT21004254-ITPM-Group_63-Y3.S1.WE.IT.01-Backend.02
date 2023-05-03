4b00a179b57bc36c572318b3d508e371e851d072
IT21002454 <IT21004254@my.sliit.lk>
2023-05-03 21:20:56 +0530

    Backend Done

---
+++ backend/models/Food.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const FoodSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : true
+    },
+
+    quantity : {
+        type : Number,
+        required : true
+    },
+    donator : {
+        type : String,
+        required : true
+    },
+    donator_phone : {
+        type : String,
+        required : true
+    }
+})
+
+const Food = mongoose.model('DonatedFood',FoodSchema)
+
+module.exports = Food
