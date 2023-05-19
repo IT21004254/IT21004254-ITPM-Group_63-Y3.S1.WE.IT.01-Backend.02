7067e119c29bb4651dbf33816fedb467c2bbc8b0
IT21002454 <IT21004254@my.sliit.lk>
2023-05-19 19:19:36 +0530

    Final Backend Done

---
+++ models/Cloth.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+const ClothesSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : true
+    },
+	category : {
+        type : String,
+        required : true
+    },
+    material : {
+        type : String,
+        required : true
+    },
+    color : {
+        type : String,
+        required : true
+    },
+    quantity : {
+        type : Number,
+        required : true
+    },
+    donator : {
+        type : String,
+        required : true
+    },
+    donator_phone : {
+        type : String,
+        required : true
+    },
+    address : {
+        type : String,
+        required : true
+    }
+   
+})
+
+const Clothes = mongoose.model('DonatedClothes', ClothesSchema)
+
+module.exports = Clothes
